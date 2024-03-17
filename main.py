80db045a570aa26e5a3aa10062225890e8a717f9
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-03-18 00:12:18 +0900

    コメント追加

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ from request_handler import RequestHandler as RequestHandlerCls
 
 
 def main(args, options):
+    # ポートを確定させる
     fzf_port = utils.find_free_port()
     server_port = utils.find_free_port()
+
+    # fzfのプロセスを開始
+    # 実際は必要なsrcやbindなどをアプリ側で作って渡す
     fzf = Fzf(fzf_port)
     fzf.start_asyn(server_port)
+
+    # serverを起動する
     RequestHandlerCls.set_fzf(fzf)
     server = InternalServer(RequestHandlerCls, server_port)
     server.start()
+
+    # fzfの出力を取得する
     stdout = fzf.communicate()
     print(stdout)
 
